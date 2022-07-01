8cb5279762f2908438c6436ca5cd24c99f67c878
Konas18 <konas2508@gmail.com>
2022-07-01 15:43:15 +0300

    Web

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
-
-gulp.task('backend', shell.task(['java -jar ./server/serverstm-0.1-all.jar']))
-
-gulp.task('serve', () => {
-  return gulp.src('./').pipe(shell(['ng serve --open']))
-})
-
-gulp.task('default', gulp.parallel('backend', 'serve'))
+const uglify = require('gulp-uglify');
+// gulp.task('backend', shell.task(['java -jar ./server/serverstm-0.1-all.jar']))
+//
+// gulp.task('serve', () => {
+//   return gulp.src('./').pipe(shell(['ng serve --open']))
+// })
+//
+// gulp.task('default', gulp.parallel('backend', 'serve'))
